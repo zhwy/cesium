215a82521cde391a3c846b3baca93823bc7680c8
zhwy <498971093@qq.com>
2026-07-13 15:04:35 +0800

    custom worker测试

--- Apps/Demos/ForTest/worker_test.js
+++ Apps/Demos/ForTest/worker_test.js
@@ -1,34 +1,8 @@
-define(function () {
-  "use strict";
+import createTaskProcessorWorker from "../../../Build/CesiumUnminified/Workers/createTaskProcessorWorker.js";
 
-  const test = (e) => {
-    console.log("got", e);
-    try {
-      return Promise.resolve("ok!");
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+function workerTest(parameters, transferableObjects) {
+  console.log({ parameters, transferableObjects });
+  return "ok!";
+}
 
-  function worker_test(event) {
-    const data = event.data;
-    const promise = test(data.parameters);
-    const responseMessage = {
-      id: data.id,
-      result: undefined,
-      error: undefined,
-    };
-    return promise
-      .then((res) => {
-        responseMessage.result = res;
-      })
-      .catch((e) => {
-        responseMessage.error = e;
-      })
-      .finally(() => {
-        postMessage(responseMessage);
-      });
-  }
-
-  return worker_test;
-});
+export default createTaskProcessorWorker(workerTest);
